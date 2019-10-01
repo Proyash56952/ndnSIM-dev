42633a104f8991f6658be80c663db48ccc0b2843
tarannum islam <tisla011@fiu.edu>
2019-10-01 14:52:53 -0400

    added l3tracer correctly more modification with distance awaits

--- examples/v2v-loss-measure.cpp
+++ examples/v2v-loss-measure.cpp
@@ -226,7 +226,7 @@ int main (int argc, char *argv[])
   Simulator::Stop (Seconds(20));
   
   // Tracer: will be saved in ns-3 folder
-  s3::ndn::L3RateTracer::InstallAll("rate-trace.txt", Seconds(0.25));
+  ns3::ndn::L3RateTracer::InstallAll("rate-trace.txt", Seconds(0.25));
   L2RateTracer::InstallAll("packet-drop.txt", Seconds(0.5));
   ns3::ndn::AppDelayTracer::InstallAll("app-delays-trace.txt");
   
